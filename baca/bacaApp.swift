f273defc46eea05d7ab7031e799abf9a0098dd3d
mylo <milopadma@yahoo.com>
2024-09-23 14:33:34 +0800

    Initial Commit

---
+++ baca/bacaApp.swift
@@ -0,0 +1,17 @@
+//
+//  bacaApp.swift
+//  baca
+//
+//  Created by mylo on 9/23/24.
+//
+
+import SwiftUI
+
+@main
+struct bacaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ bacaTests/bacaTests.swift
@@ -0,0 +1,17 @@
+//
+//  bacaTests.swift
+//  bacaTests
+//
+//  Created by mylo on 9/23/24.
+//
+
+import Testing
+@testable import baca
+
+struct bacaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
